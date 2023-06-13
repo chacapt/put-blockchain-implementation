26f6d91c12f1b3fb5a04b32a0b518984bf36c5c0
Filip <fil.kokosza@gmail.com>
2023-06-13 23:40:46 +0200

    Added blockchain console user interface

--- main.py
+++ main.py
@@ -95,9 +95,8 @@ class Blockchain:
         new_block.prev_hash = self.chain[-1].hash
         new_block.calculate_proof_of_work()
         self.chain.append(new_block)
-        return new_block.hash
 
-    def get_hash(self):
+    def get_tail_hash(self):
         return self.chain[-1].hash
 
     def remove_blockchain(self):
@@ -128,6 +127,10 @@ class NetworkNodes:
         if block.calc_hash() == block.hash:
             return True
 
+    def mine_block(self, block: Block):
+        self.blockchain.add_block(block)
+        return self
+
 
 def initialize_user_list():
     users = [(User("Adam", '123'))]
@@ -144,6 +147,7 @@ def print_all_blocks(block_list: Blockchain):
 
 
 def sing_up(network: NetworkNodes):
+    os.system('cls')
     name = input("Enter username: ")
     pwd_hash = ''
     while 1:
@@ -154,12 +158,15 @@ def sing_up(network: NetworkNodes):
             break
         else:
             sing_up_error_msg("Passwords are not the same!")
+            os.system('cls')
     while 1:
         if network.get_user(name):
             network.users.append(User(name, pwd_hash))
             sing_up_error_msg("You have registered successfully!")
+            os.system('cls')
             break
         else:
+            os.system('cls')
             print("Name is taken, please enter again!")
             name = input("Enter username: ")
 
@@ -173,6 +180,7 @@ def sing_up_error_msg(arg0):
 def login(network: NetworkNodes):
     user = User('', '')
     while 1:
+        os.system('cls')
         name = input("Enter username: ")
         password = input("Enter password: ")
         pwd_hash = pwd_hash = hashlib.md5(password.encode()).hexdigest()
@@ -180,11 +188,11 @@ def login(network: NetworkNodes):
         if [user for user in network.users if user.name == name]:
             if user.token == pwd_hash:
                 print("Welcome again!")
-                break
+                return user
         else:
             print("User does not exist or given password is wrong! \n")
-            if input("Do you want to exit? (yes/no): ").lower() == 'yes':
-                break
+            if input("Do you wish to return? (yes/no): ").lower() == 'yes':
+                return None
 
 
 def load_blockchain_network():
@@ -216,13 +224,15 @@ def login_menu(blockchain_network):
             case 1:
                 sing_up(blockchain_network)
             case 2:
-                login(blockchain_network)
-                return True
+                user = login(blockchain_network)
+                if user != None:
+                    return user
             case 3:
                 login_menu_msg("Exiting...")
                 break
             case _:
                 login_menu_msg("Wrong Choice!")
+        os.system('cls')
 
 
 def login_menu_msg(arg0):
@@ -230,3 +240,121 @@ def login_menu_msg(arg0):
     print(arg0)
     time.sleep(0.7)
     os.system('cls')
+
+
+def add_transaction(blockchain_network: NetworkNodes, currentUser: User):
+    os.system('cls')
+    print("----- List of recievers -----")
+    for user in enumerate(blockchain_network.users):
+        if user[1].name != currentUser:
+            print(user[0], user[1].name)
+    print("-----------------------------")
+    choice = int(input("Pick position of the recipient: "))
+    amount = float(input("Amount of the transaction: "))
+    transaction = Transaction(
+        currentUser, blockchain_network.users[choice-1], amount)
+    os.system('cls')
+    print("Verifying new transaction...")
+    time.sleep(0.8)
+    if blockchain_network.verify_transaction(transaction):
+        return transaction
+    else:
+        return None
+
+
+def show_transactions(transactions: list[Transaction]):
+    os.system('cls')
+    print("----- List of non-mined transactions -----")
+    for tx in transactions:
+        print(tx)
+    print("------------------------------------------")
+    while 1:
+        if input("Return? (yes/no): ").lower() == 'yes':
+            os.system('cls')
+            return
+
+
+def mine_block(blockchain_network: NetworkNodes, transactions: list[Transaction]):
+    block = Block(transactions, blockchain_network.blockchain.get_tail_hash())
+    os.system('cls')
+    print("Mining the new block...")
+    blockchain_network_new: NetworkNodes = blockchain_network.mine_block(block)
+    mine_msg("Block has been mined", 0.5)
+    if blockchain_network.verify_block(blockchain_network_new.blockchain.chain[-1]):
+        mine_msg("Block is being verified...", 0.5)
+        mine_msg(
+            "Block has been successfully verified and added to the Blockchain", 0.3)
+        return blockchain_network_new
+    else:
+        mine_msg("Block could not be verified!", 0.5)
+
+
+def mine_msg(arg0, arg1):
+    os.system('cls')
+    print(arg0)
+    time.sleep(arg1)
+
+
+def blockchain_user_menu(blockchain_network: NetworkNodes, user: User):
+    transactions: list[Transaction] = []
+    print('hello to the blockchain')
+    while 1:
+        print("Currently logged as: ", user.name,
+              "\t\tTransactions: ", len(transactions))
+        print("---------    Blockchain User Interface   ---------")
+        print("1. Show current blockchain")
+        print("2. Add new transaction")
+        print("3. Show transactions not in blockchain")
+        print("4. Mine block of current transactions")
+        print("5. Logout")
+        choice = input("Enter your choice (1-5): ")
+
+        match choice:
+            case 1:
+                os.system('cls')
+                print(blockchain_network.blockchain)
+                while 1:
+                    if input("Return (yes/no): ").lower() == 'yes':
+                        break
+                os.system('cls')
+            case 2:
+                tx = add_transaction(blockchain_network, user)
+                if tx != None:
+                    transactions.append(tx)
+                    print("Transaction has been successfully verified and added!")
+                    time.sleep(0.7)
+                else:
+                    print("Transaction could not be verified!")
+                    time.sleep(0.7)
+            case 3:
+                show_transactions(transactions)
+            case 4:
+                mine_block(blockchain_network, transactions)
+                transactions.clear()
+                os.system('cls')
+            case 5:
+                login_menu_msg("Logging out...")
+                break
+            case _:
+                login_menu_msg("Wrong Choice!")
+        os.system('cls')
+
+
+def save_objects(blockchain: Blockchain, users: list[User]):
+    blockchain_file = "blockchain.pkl"
+    users_file = "users.pkl"
+    with open(blockchain_file, "wb") as file:
+        pickle.dump(blockchain, file)
+
+    with open(users_file, "wb") as file:
+        pickle.dump(users, file)
+
+
+if __name__ == "__main__":
+    users, blockchain = load_blockchain_network()
+    blockchain_network = NetworkNodes(users, blockchain)
+    logged_user = login_menu(blockchain_network)
+    if logged_user != None:
+        blockchain_user_menu(blockchain_network, logged_user)
+    else:
+        exit()
